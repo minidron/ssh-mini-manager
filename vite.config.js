bc6f629ea2942d7b9bd4d1d834bd7f4143bc3938
Pavel Alekin <pavel.alekin@gmail.com>
2022-03-30 23:16:24 +0300

    Initial configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
+import { fileURLToPath, URL } from "url"
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.ELECTRON == 'true' ? './' : '.',
-  plugins: [vue()]
+  base: process.env.ELECTRON == 'true' ? './' : '/',
+
+  plugins: [vue()],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/scss/variables.scss";
+        `
+      }
+    }
+  },
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  }
 })
